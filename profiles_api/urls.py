fb5f7fbf7811411a40d0dd96bade3d663b417e26
hungltph03955 <hungltph03955@fpt.edu.vn>
2019-10-28 00:25:46 +0700

    47

---
+++ profiles_api/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from profiles_api import views
+
+router = DefaultRouter()
+router.register('hello-viewsets', views.HelloViewSet, base_name='hello-viewsets')
+router.register('profile', views.UserProfileViewSet)
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
+]
