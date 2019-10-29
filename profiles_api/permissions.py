cb3447d39c1021a21fef2786b86c9fb8d96392ef
hungltph03955 <hungltph03955@fpt.edu.vn>
2019-10-30 00:16:29 +0700

    code

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 
-
 class UpdateOwnProfile(permissions.BasePermission):
     """ Allo user to edit their own profile """
 
@@ -10,3 +9,11 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """ Allow users to update their own status """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,10 +6,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -25,11 +27,11 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs',
         ]
 
-        return Response({'message' : 'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
     def post(self, request):
         """ Create a hello message with our name """
-        serializer = self.serializer_class(data = request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name}'
@@ -52,6 +54,7 @@ class HelloApiView(APIView):
         """ Delete an object """
         return Response({'method': 'DELETE'})
 
+
 class HelloViewSet(viewsets.ViewSet):
     """ Test API ViewSet """
     serializer_class = serializers.HelloSerializer
@@ -59,7 +62,7 @@ class HelloViewSet(viewsets.ViewSet):
     def list(self, request):
         """ Return a hello message """
 
-        a_viewset = [
+        view_set = [
             'Uses actions (list, create,, retrieve, update, partial_update)',
             'Automatically maps to URLs using Routers',
             'Provides more functionality with less code',
@@ -69,7 +72,7 @@ class HelloViewSet(viewsets.ViewSet):
 
     def create(self, request):
         """ Create a new hello message """
-        serializer = self.serializer_class(data = request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name}'
@@ -96,6 +99,7 @@ class HelloViewSet(viewsets.ViewSet):
         """ Handle removing an object """
         return Response({'http_method': 'DELETE'})
 
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     """ Handle creating and Updating profile """
     serializer_class = serializers.UserProfileSerializer
@@ -105,6 +109,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+
 class UserLoginApiView(ObtainAuthToken):
     """ Handle creating user authentication tokens """
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """ Handles creating, reading and updating profile feed items """
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """ Sets the user profile to the logged in user """
+        serializer.save(user_profile=self.request.user)
